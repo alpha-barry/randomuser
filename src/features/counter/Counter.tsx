e03f39d3f471985575335c12b632a336a27fac90
Alpha-Oumar BARRY <alpha-oumar.barry@outlook.com>
2022-12-07 18:50:31 +0100

    add material ui card

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,36 +1,58 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import Typography from '@mui/material/Typography';
 import { useGetRandomUserQuery, useGetUserBySeedQuery } from '../../services/randomuser'
 import styles from './Counter.module.css';
 
 export function Counter() {
   const { data, error, isLoading } = useGetRandomUserQuery()
 
+  const bull = (
+    <Box
+      component="span"
+      sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    >
+      •
+    </Box>
+  );
+
   const randomUsers = data?.results.map((randomUser: any) => {
-      return (<div key={randomUser.login.uuid}>
-        <div>
-          {randomUser.name.first}
-        </div>
-        <div>
-          {randomUser.name.last}
-      </div>
-      <br/>
-      <div>
-        
-      </div>
-      </div>);
+      return (<Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Nom: {randomUser.name.first}
+        </Typography>
+        <Typography variant="h5" component="div">
+          Prénom: {randomUser.name.first}
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          adjective
+        </Typography>
+      </CardContent>
+      <CardActions>
+      <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+          </IconButton>
+      </CardActions>
+    </Card>  );
   });
 
 
   return (
     <div>
       {error ? (
-        <>Oh no, there was an error</>
+        <>there was an error</>
       ) : isLoading ? (
         <>Loading...</>
       ) : data ? (
         <>
-          {isLoading ? <div>Loading...</div>: <div>{randomUsers}</div>}
-        </>
+        {randomUsers}
+      </>
       ) : null}
     </div>
   );
